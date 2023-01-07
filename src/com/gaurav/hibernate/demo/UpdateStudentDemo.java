3e2b4d569c77b7bf96580714c19b59ee0c042230
Gaurav Sahal <gaurav.sahal10@gmail.com>
2023-01-07 12:06:19 +0530

    Added UpdateStudentDemo

---
+++ src/com/gaurav/hibernate/demo/UpdateStudentDemo.java
@@ -0,0 +1,60 @@
+package com.gaurav.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.gaurav.hibernate.demo.entity.Student;
+
+public class UpdateStudentDemo {
+
+	public static void main(String[] args) {
+
+		//create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+		//create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			int studentId = 1;
+			
+			// now get a new session and start transaction
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			
+			//retrieve student based on the id: primary key
+			System.out.println("\nGetting student with id: "+ studentId);
+			
+			Student myStudent = session.get(Student.class, studentId);
+			
+			System.out.println("Updating Student...");
+			myStudent.setFirstName("Scooby");
+			
+			//commit the transaction
+			session.getTransaction().commit();
+			
+			//New code
+			
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			
+			//update email for all students
+			System.out.println("Update email for all students");
+			
+			session.createQuery("update Student set email='foo@gmail.com'").executeUpdate();
+			
+			//commit the transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			factory.close();
+		}
+	}
+
+}
