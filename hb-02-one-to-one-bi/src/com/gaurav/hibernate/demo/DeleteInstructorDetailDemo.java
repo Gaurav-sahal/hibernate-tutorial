f2dd4cc2bfa270b187755bfe68d2f3d3f033f157
Gaurav Sahal <gaurav.sahal10@gmail.com>
2023-01-15 15:37:10 +0530

    Added DeleteInstructorDetailDemo

---
+++ hb-02-one-to-one-bi/src/com/gaurav/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -0,0 +1,67 @@
+package com.gaurav.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.gaurav.hibernate.demo.entity.Instructor;
+import com.gaurav.hibernate.demo.entity.InstructorDetail;
+import com.gaurav.hibernate.demo.entity.Student;
+
+public class DeleteInstructorDetailDemo {
+
+	public static void main(String[] args) {
+
+		//create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.buildSessionFactory();
+		
+		//create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+				
+			//start a transaction
+			session.beginTransaction();
+		
+			// get the instructor detail object
+			int theId = 3;
+			
+			InstructorDetail tempInstructorDetail = 
+					session.get(InstructorDetail.class, theId);
+			
+			//print the instructor detail
+			System.out.println("tempInstructorDetail: "+ tempInstructorDetail);
+			
+			//print the associated instructor
+			System.out.println("the associated Instructor: "+ tempInstructorDetail.getInstructor());
+			
+			// now let's delete the instructor detail
+			System.out.println("Deleting tempInstructorDetail: "+ tempInstructorDetail);
+			
+			//remove the associated object reference
+			//break bi-directional link
+			tempInstructorDetail.getInstructor().setInstructorDetail(null);
+			
+			session.delete(tempInstructorDetail);
+			
+			//commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		catch(Exception exe) {
+			exe.printStackTrace();
+		}
+		finally {
+			//handle connection leak issue
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
